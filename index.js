0b9cccfb8e76402feff37a1dc250f39c9bed9dd8
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-01-14 13:49:17 +0530

    :tada: configured the rate limiter.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 90, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  limit: 90, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
